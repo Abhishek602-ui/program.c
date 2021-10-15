3da75b2df7d02b6ef3faab37b33ea7eb5bc5083a
Abhishek602-ui <92527633+Abhishek602-ui@users.noreply.github.com>
2021-10-15 15:10:53 +0530

    Add files via upload

---
+++ divide and ouetent.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float b1,b2,h;
+	float area;
+	printf("Enter the size of two bases and hights of the trapezium")
+	scanf("%f%f%f",&b1,&b2,&h);
+	printf("Area of trapezium is%3f",area);
+return 0;
+
+}
